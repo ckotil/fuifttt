81307a4d8d12d04e6b0548d01eeda8cf6d878e74
ckotil2 <ckotil@indiana.edu>
2018-11-26 22:22:38 -0800

    Update fuifttt.sh
    Fixed a typo. Made phone number an argument.

--- fuifttt.sh
+++ fuifttt.sh
@@ -1,16 +1,17 @@
-!#/bin/bash
+#!/bin/bash
+PHONENUMBER="$1"
 for i in i80 us50 sr88; do
     if curl -s http://www.dot.ca.gov/hq/roadinfo/display.php?page=$i | grep -v "CLOSED TO SINGLE DRIVE"  | grep -i closed >/dev/null ;
     then echo $i closed; rm -f /tmp/$i.open;
       if [[ -f /tmp/$i.closed ]];
         then echo mail\ already\ sent;
         else touch /tmp/$i.closed;
-          curl -s http://www.dot.ca.gov/hq/roadinfo/display.php?page=$i  | grep -i closed | tr -d \\r  | mailx <phonenumber>@vtext.com;
+          curl -s http://www.dot.ca.gov/hq/roadinfo/display.php?page=$i  | grep -i closed | tr -d \\r  | mailx $PHONENUMBER@vtext.com;
       fi ;
     else echo $i opened; rm -f /tmp/$i.closed;
       if [[ -f /tmp/$i.open ]];
         then echo mail\ already\ sent;
-        else touch /tmp/$i.open; echo "$i Open\!" | tr -d \\r  | mailx <phonenumber>@vtext.com;
+        else touch /tmp/$i.open; echo "$i Open\!" | tr -d \\r  | mailx $PHONENUMBER@vtext.com;
       fi;
     fi;
  done
